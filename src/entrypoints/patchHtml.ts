cb069543db1f5ef84d12d281bf1b61b60234ff5e
igsr5 <igsr5.dev@gmail.com>
2023-07-08 13:17:25 +0900

    Patch HTML with Open Graph meta tags

--- src/entrypoints/patchHtml.ts
+++ src/entrypoints/patchHtml.ts
@@ -2,11 +2,14 @@ import patchHtmlFile from "../patchHtml/patchHtml";
 
 const filePath = "public/index.html";
 const metaTags = `
-  <meta charset="UTF-8">
-  <meta name="description" content="Free Web tutorials">
-  <meta name="keywords" content="HTML, CSS, JavaScript">
-  <meta name="author" content="John Doe">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta property="og:title" content="Sora Ichigo - Web Developer" />
+<meta property="og:description" content="Sora Ichigo is a Web Developer based in Tokyo, Kanto, JP. Currently working as a Software Engineer at Wantedly, Inc. Skilled in Frontend and Backend Development, Infrastructure, and Knowledge in various fields." />
+<meta property="og:url" content="https://igsr5.github.io/my-resume/" />
+<meta property="og:image" content="https://res.cloudinary.com/drb9hgnv3/image/upload/v1688789772/Screen_Shot_2023-07-08_at_13.15.28_nixbnw.png" />
+<meta property="og:type" content="website" />
+<meta property="og:locale" content="ja_JP" />
 `;
 
-patchHtmlFile(filePath, metaTags);
+(async () => {
+  await patchHtmlFile(filePath, metaTags);
+})();
