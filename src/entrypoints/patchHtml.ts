05c45b08aa1ffcfc1e86e5c7df87c3517460cfb5
igsr5 <igsr5.dev@gmail.com>
2023-07-08 22:39:52 +0900

    Add Google tag (gtag.js) to patchHtml.ts entrypoint

--- src/entrypoints/patchHtml.ts
+++ src/entrypoints/patchHtml.ts
@@ -12,6 +12,15 @@ const metaTags = `
 `;
 
 const scriptTags = `
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RX5CTP05JJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RX5CTP05JJ');
+</script>
 `;
 
 (async () => {
